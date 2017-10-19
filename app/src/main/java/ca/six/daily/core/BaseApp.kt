b79c4832cdd3fb33416ded67047d20b64e062e38
songzhw <songzhw2012@gmail.com>
2017-10-18 21:30:16 -0400

    BaseApp: add src. Test failed again

--- app/src/main/java/ca/six/daily/core/BaseApp.kt
+++ app/src/main/java/ca/six/daily/core/BaseApp.kt
@@ -1,6 +1,16 @@
 package ca.six.daily.core
 
 import android.app.Application
+import android.content.Context
+import kotlin.properties.Delegates
 
 class BaseApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        app = this;
+    }
+
+    companion object {
+        var app: Context by Delegates.notNull<Context>()
+    }
 }
