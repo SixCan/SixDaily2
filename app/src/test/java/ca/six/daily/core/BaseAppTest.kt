918f56af7f7d28949e3b2cde9d1a24fa1e4506fe
songzhw <songzhw2012@gmail.com>
2017-10-18 21:31:20 -0400

    BaseApp: test passed

--- app/src/test/java/ca/six/daily/core/BaseAppTest.kt
+++ app/src/test/java/ca/six/daily/core/BaseAppTest.kt
@@ -1,12 +1,19 @@
 package ca.six.daily.core
 
-import org.junit.Assert.*
+import android.os.Build
+import ca.six.daily.BuildConfig
+import org.junit.Assert.assertNotNull
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP), application = BaseApp::class)
 class BaseAppTest {
 
     @Test
-    fun testContextNotNull(){
+    fun testContextNotNull() {
         assertNotNull(BaseApp.app)
     }
 
